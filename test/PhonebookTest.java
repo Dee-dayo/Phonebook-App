696d0ee981c89ef63bc314615fe65194761974cc
Dee-dayo <darhyor2050@gmail.com>
2024-02-01 08:41:47 +0100

    Phonebook

--- test/PhonebookTest.java
+++ test/PhonebookTest.java
@@ -13,6 +13,16 @@ public class PhonebookTest {
     @Test
     public void testThatItShowsAWelcomePage(){
         PhoneBook phoneBook  = new PhoneBook();
-        assertEquals("PHONEBOOK\n1. Create a contact\n2. View Saved Contacts", phoneBook.welcomePage());
+        String menu = "\nPHONEBOOK\n1. Save a contact\n2. View Saved Contacts\n3. Search Saved Contacts\n4. Delete a contact\n5. Exit";
+        assertEquals(menu, phoneBook.welcomePage());
+    }
+
+
+    @Test
+    public void testThatTheApplicationCanCreateContact(){
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.createContact("dayo", "08172581627");
+        
+
     }
 }
